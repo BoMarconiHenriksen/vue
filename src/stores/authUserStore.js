4c42b690c56a190ba2fdcade8e5468f79b4b9273
Bo Henriksen <bomarconi@protonmail.com>
2022-07-30 09:27:01 +0200

    for getters use mapState with th eoption api

--- src/stores/authUserStore.js
+++ src/stores/authUserStore.js
@@ -3,7 +3,10 @@ import { defineStore } from 'pinia';
 export const useAuthUserStore = defineStore("AuthUserStore", { 
   state: () => {
     return {
-      username: "bh_io"
+      // username: "bh_io"
     }
-  }
-})
+  },
+  getters: {
+    username: () => "bh_io"
+  },
+});
