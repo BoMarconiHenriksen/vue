8e2636d3d55c8256fcab937720526609e7742b79
Bo Henriksen <bomarconi@protonmail.com>
2022-07-30 09:08:52 +0200

    change the name of state with option api

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,7 +7,9 @@ import { mapState } from "pinia";
 export default {
   components: { CartWidget },
   computed: {
-    ...mapState( useAuthUserStore, ["username"] ),
+    ...mapState( useAuthUserStore, {
+      user: "username"
+    }),
   },
 };
 </script>
@@ -19,7 +21,7 @@ export default {
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div>
-      <span class="mr-5">{{ username }}</span>
+      <span class="mr-5">{{ user }}</span>
       <CartWidget class="inline-block"/>
     </div>
   </header>
