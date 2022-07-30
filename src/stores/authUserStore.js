0b9091718e2a4c2feb754966ba62f42b0983eaa9
Bo Henriksen <bomarconi@protonmail.com>
2022-07-30 09:31:29 +0200

    removing getters

--- src/stores/authUserStore.js
+++ src/stores/authUserStore.js
@@ -3,10 +3,7 @@ import { defineStore } from 'pinia';
 export const useAuthUserStore = defineStore("AuthUserStore", { 
   state: () => {
     return {
-      // username: "bh_io"
+      username: "bh_io"
     }
-  },
-  getters: {
-    username: () => "bh_io"
-  },
-});
+  }
+})
